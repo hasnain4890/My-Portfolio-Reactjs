e23f3e943d72be89f110b6e559f6dc9d12e0256b
MacBook Pro <macbookpro@MacBooks-MBP.mshome.net>
2024-08-01 17:45:08 +0500

    Adding dot

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -8,7 +8,7 @@ const Features = () => {
       id="features"
       className="w-full py-20 border-b-[1px] border-b-black"
     >
-      <Title title="Features" des="What I Do" />
+      <Title title="Features" des="What I Do." />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         {featuresData.map((item) => (
           <Card item={item} key={item.id} />
