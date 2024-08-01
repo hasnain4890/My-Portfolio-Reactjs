b3a9c848ff24c75bcd765060e016dce4c8f3d787
hasnain4890 <hasnainahmad4890@gmail.com>
2024-08-01 21:12:10 +0500

    Adding Two new skills react native and docker and also correcting typescript spelling

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
 const Skills = () => {
   return (
@@ -41,6 +41,19 @@ const Skills = () => {
                 <span className="absolute -top-7 right-0">90%</span>
               </motion.span>
             </span>
+          </div>{" "}
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">React Native</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">90%</span>
+              </motion.span>
+            </span>
           </div>
           <div className="overflow-x-hidden">
             <p className="text-sm uppercase font-medium">JavaScript</p>
@@ -56,7 +69,7 @@ const Skills = () => {
             </span>
           </div>
           <div className="overflow-x-hidden">
-            <p className="text-sm uppercase font-medium">TypesSript</p>
+            <p className="text-sm uppercase font-medium">TypeScript</p>
             <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
               <motion.span
                 initial={{ x: "-100%", opacity: 0 }}
@@ -156,11 +169,24 @@ const Skills = () => {
                 <span className="absolute -top-7 right-0">75%</span>
               </motion.span>
             </span>
+          </div>{" "}
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">Docker</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">75%</span>
+              </motion.span>
+            </span>
           </div>
         </div>
       </div>
     </motion.div>
   );
-}
+};
 
-export default Skills
+export default Skills;
